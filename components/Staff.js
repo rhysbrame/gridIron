6ae8e32c9f52f24aec12351377f695f722e471dd
rhysbrame <25747279+rhysbrame@users.noreply.github.com>
2022-04-25 13:03:11 +0100

    Styling for Staff section

--- components/Staff.js
+++ components/Staff.js
@@ -8,17 +8,24 @@ const Staff = (props) => {
       <>
         <div className="staff-container">
           <h1 className="heading-1">Staff</h1>
-          <h2 className="heading-2">
-            Head Coach:&nbsp;
-            {team.HeadCoach}
-          </h2>
-          <ul>
-            <li>Defensive Coordinator:&nbsp;{team.DefensiveCoordinator}</li>
-            <li>Defensive Scheme:&nbsp;{team.DefensiveScheme}</li>
-            <li>Offensive Coordinator:&nbsp;{team.OffensiveCoordinator}</li>
-            <li>Offensive Scheme:&nbsp;{team.OffensiveScheme}</li>
-            <li>Special Teams Coach:&nbsp;{team.SpecialTeamsCoach}</li>
-          </ul>
+          <h3 className="staff-container__head-title">
+            Head Coach:&nbsp;{team.HeadCoach}
+          </h3>
+          <h5 className="staff-container__coach-title">
+            Defensive Coordinator:&nbsp;{team.DefensiveCoordinator}
+          </h5>
+          <h5 className="staff-container__coach-title">
+            Defensive Scheme:&nbsp;{team.DefensiveScheme}
+          </h5>
+          <h5 className="staff-container__coach-title">
+            Offensive Coordinator:&nbsp;{team.OffensiveCoordinator}
+          </h5>
+          <h5 className="staff-container__coach-title">
+            Offensive Scheme:&nbsp;{team.OffensiveScheme}
+          </h5>
+          <h5 className="staff-container__coach-title">
+            Special Teams Coach:&nbsp;{team.SpecialTeamsCoach}
+          </h5>
         </div>
       </>
     );
