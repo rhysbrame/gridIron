911735352bbb0bb1e03066b12e51302b2a0f9e64
rhysbrame <25747279+rhysbrame@users.noreply.github.com>
2022-04-29 14:59:41 +0100

    Fixed webpack config for production deployment on vercel

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,12 @@ const HTMLWebpackPlugin = require('html-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    main: path.resolve(__dirname, './src/index.js'),
+  },
   output: {
-    path: path.resolve(__dirname, '/dist'),
-    filename: 'index_bundle.js',
+    path: path.resolve(__dirname, './dist'),
+    filename: '[name].bundle.js',
   },
   module: {
     rules: [
