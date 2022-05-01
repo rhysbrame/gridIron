3a72cc14e249cb8e14b262b7fe35554484411ecc
rhysbrame <25747279+rhysbrame@users.noreply.github.com>
2022-05-01 17:56:23 +0100

    Updated API calls

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import TeamSelector from '../components/TeamSelector';
 import Stadium from '../components/Stadium';
 import Staff from '../components/Staff';
@@ -8,26 +8,10 @@ import Roster from '../components/Roster';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
-// Change this Teams object to a call on the 3rd party API for up to date information.
-import Teams from '../public/Teams.json';
-
-const NFL = {
-  Key: 'NFL',
-  Name: 'Select an NFL Team...',
-  PrimaryColor: '808080',
-  SecondaryColor: 'FFFFFF',
-  TertiaryColor: '000000',
-  WikipediaLogoUrl:
-    'https://upload.wikimedia.org/wikipedia/en/a/a2/National_Football_League_logo.svg',
-  WikipediaWordMarkUrl:
-    'https://upload.wikimedia.org/wikipedia/en/b/b4/100_NFL_seasons_logo.svg',
-};
-
-Teams.unshift(NFL);
-
 const App = () => {
+  const [teams, setTeams] = useState([]);
   const [chosenTeam, setChosenTeam] = useState({
-    Key: null,
+    Key: 'NFL',
     Name: 'Select an NFL Team',
     PrimaryColor: '808080',
     SecondaryColor: 'FFFFFF',
@@ -37,9 +21,45 @@ const App = () => {
     WikipediaWordMarkUrl:
       'https://upload.wikimedia.org/wikipedia/en/b/b4/100_NFL_seasons_logo.svg',
   });
+
+  const TEAMS_URL =
+    process.env.NODE_ENV === 'development'
+      ? 'Teams.json'
+      : `https://api.sportsdata.io/v3/nfl/scores/json/Teams?key=${process.env.SPORTSDATA_API_KEY}`;
+
+  useEffect(() => {
+    fetch(TEAMS_URL)
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error('No Teams data');
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setTeams(data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  });
+
+  // const NFL = {
+  //   Key: 'NFL',
+  //   Name: 'Select an NFL Team...',
+  //   PrimaryColor: '808080',
+  //   SecondaryColor: 'FFFFFF',
+  //   TertiaryColor: '000000',
+  //   WikipediaLogoUrl:
+  //     'https://upload.wikimedia.org/wikipedia/en/a/a2/National_Football_League_logo.svg',
+  //   WikipediaWordMarkUrl:
+  //     'https://upload.wikimedia.org/wikipedia/en/b/b4/100_NFL_seasons_logo.svg',
+  // };
+
+  // teams.unshift(NFL);
+
   const handleSelectTeam = (e) => {
     const key = e.target.value;
-    const selectedTeam = Teams.find((team) => {
+    const selectedTeam = teams.find((team) => {
       if (team.Key === key) return team;
     });
     setChosenTeam(selectedTeam);
@@ -63,7 +83,7 @@ const App = () => {
     <div className="background-container">
       <div className="main-container">
         <Header data={chosenTeam} />
-        <TeamSelector teams={Teams} onChange={handleSelectTeam} />
+        <TeamSelector teams={teams} onChange={handleSelectTeam} />
         <News teamID={chosenTeam.Key} />
         <div className="sss-container">
           <Stadium stadium={chosenTeam.StadiumDetails} />
